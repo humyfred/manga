cb167f0793d1ccc23482a5c285e3a2f8b36b1bd2
humyfred <513407656@qq.com>
2017-01-02 17:10:12 +0800

    cartoon-card

--- reducers/index.js
+++ reducers/index.js
@@ -1,19 +1,18 @@
 import {combineReducers } from 'redux'
 import frames from './frames'
 import cards from './cards'
-import viewDetailState from './viewDetailState'
-import viewListState from './viewListState'
+import indexViewState from './content-view/index-view-state'
+import contentViewState from './content-view/content-view-state'
 import cartoonCardList from './cartoon-card/cartoon-card-list'
-import viewDetailImgs from './viewDetailImgs'
+
 import { routerReducer } from 'react-router-redux'
 // Add the reducer to your store on the `routing` key
 const reducers = combineReducers({
     cartoonCardList,
     frames,
     cards,
-    viewDetailState,
-    viewListState,
-    viewDetailImgs,
+    contentViewState,
+    indexViewState,
     routing:routerReducer
 })
 
