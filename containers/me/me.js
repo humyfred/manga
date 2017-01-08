9ba2086bd1ff1a080ec165273767380cb7bb1740
humyfred <513407656@qq.com>
2017-01-08 20:38:32 +0800

    new files

--- containers/me/me.js
+++ containers/me/me.js
@@ -1,20 +1,27 @@
 'use strict';
 import { connect } from 'react-redux';
 import { toViewDetail, toViewList } from '../../actions/commons';
+import {toViewSubscribe,toViewPost, toViewLike } from '../../actions/me'
 import the_me from '../../components/commons/me/me';
 
 
 
 const mapStateToProps = (state) => {
 
-  return {me:state.me.me,tabContent:state.me.tabContent};
+  return {me:state.me.me,tabContent:state.me.tabContent,nav:state.me.nav};
 };
 
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    back: () => {
-      dispatch(toViewList());
+    toViewSubscribe: () =>{
+      dispatch(toViewSubscribe());
+    },
+    toViewPost: () =>{
+      dispatch(toViewPost());
+    },
+    toViewLike: () =>{
+      dispatch(toViewLike());
     }
   };
 };
--- reducers/me/me.js
+++ reducers/me/me.js
@@ -1,18 +1,25 @@
 'use strict';
+let obj = {
+ me:{
+   avator:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1740907959,1531109126&fm=23&gp=0.jpg',
+   name:'小熊',
+   autograph:'多情自古伤离别'
+ },
+ tabContent:'1111'
+};
 
 const me = (state = [], action) => {
   switch (action.type) {
     case 'ME_SETTING':
-        return {};
+        return Object.assign(obj,{nav:{name:'subscribe'}});
+    case 'ME_VIEW_SUBSCRIBE':
+        return Object.assign(obj,{nav:{name:'subscribe'},tabContent:'1111'});
+    case 'ME_VIEW_POST':
+        return Object.assign(obj,{nav:{name:'post'},tabContent:'2222'});
+    case 'ME_VIEW_LIKE':
+        return Object.assign(obj,{nav:{name:'like'},tabContent:'3333'});
     default:
-       return {
-        me:{
-          avator:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1740907959,1531109126&fm=23&gp=0.jpg',
-          name:'小熊',
-          autograph:'多情自古伤离别'
-        },
-        tabContent:'1111'
-      };
+       return obj;
   }
 }
 
