1868e4026141d60f6eff79d7a8286318f2353453
humyfred <513407656@qq.com>
2017-01-10 18:16:30 +0800

    modify

--- containers/cartoon-card/cartoon-card-list.js
+++ containers/cartoon-card/cartoon-card-list.js
@@ -1,6 +1,6 @@
 'use strict';
 import { connect } from 'react-redux'
-import { toViewCartoon , fetchPostsTool } from '../../actions/cartoon-card'
+import { fetchPostsTool } from '../../actions/cartoon-card'
 import { toViewDetail } from '../../actions/commons';//转换场景
 import CartoonCards from '../../components/commons/cartoon-card-list/cartoon-card-list'
 import { browserHistory } from 'react-router'
