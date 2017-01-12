1ccaf74d2550446a2949eccb736bd909e29b89cb
humyfred <2681776832@qq.com>
2017-01-12 18:25:40 +0800

    post like action

--- containers/post/post-list.js
+++ containers/post/post-list.js
@@ -1,11 +1,18 @@
 'use strict';
 import { connect } from 'react-redux'
-import { fetchPosts } from '../../actions/post'
+import { fetchPosts , likePost } from '../../actions/post'
 import Posts from '../../components/commons/post-list/post-list'
 
 
 
 const mapStateToProps = (state) => {
+  if(state.postList[0].isLike){
+    return {
+      posts: state.postList ,
+      indexViewState :state.indexViewState,
+      yeah:1
+    }
+  }
   return {
     posts: state.postList ,
     indexViewState :state.indexViewState
@@ -16,6 +23,12 @@ const mapDispatchToProps = (dispatch) => {
   return {
     detail: (obj) => {
       dispatch(fetchPosts(obj));
+    },
+    like : (obj,idx) =>{
+      dispatch(likePost(obj,idx));
+    },
+    comment : (obj,idx) =>{
+      dispatch(likePost(obj,idx));
     }
   }
 }
