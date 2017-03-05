3f0a0304acc5bbc3dc1b7db8f1dce647512548aa
humyfred <513407656@qq.com>
2017-03-06 01:34:23 +0800

    loading

--- components/commons/dropdown-loading/dropdown-loading-plugin.js
+++ components/commons/dropdown-loading/dropdown-loading-plugin.js
@@ -1,35 +1,56 @@
 ;(function(window){
   'use strict';
+  function noop (){
+
+  }
+
   function dropdownLoading(dom){
 
     this._description = '<div id="dropdownLoadingDescription">下拉加载....</div>'
     this._target = dom.lastChild;
 
-    dom.lastChild.addEventListener('touchstart',this.onTouchStart.bind(this),false);
-    dom.lastChild.addEventListener('touchmove',this.onTouchMove.bind(this),false);
-    dom.lastChild.addEventListener('touchend',this.onTouchEnd.bind(this),false);
+    this.touchMove = options.touchMove || noop;
+    this.touchStart = options.touchStart || noop;
+    this.touchEnd = options.touchEnd || noop;
+
+    dom.lastChild.addEventListener('touchstart',this._touchStart.bind(this),false);
+    dom.lastChild.addEventListener('touchmove',this._touchMove.bind(this),false);
+    dom.lastChild.addEventListener('touchend',this._touchEnd.bind(this),false);
 
     dom.lastChild.before(description);
 
     this._description = document.querySelect('#dropdownLoadingDescription');
   }
 
-  dropdownLoading.prototype.onTouchStart = function(event){
+  dropdownLoading.prototype.constructor = dropdownLoading;
+
+  dropdownLoading.prototype._touchStart = function(event){
     this.startY = event.touches[0].pageY;
   }
 
-  dropdownLoading.prototype.onTouchMove = function(event){
+  dropdownLoading.prototype._touchMove = function(event){
     var currentY = event.touches[0].pageY;
+    this._gap = currentY - this.startY;
+    if(this._gap){
 
+      this._target.style.transform = 'translate3d(0,' + this._gap + 'px,0)';
+    }
+    this.touchMove.call(this, event);
   }
 
-  dropdownLoading.prototype.onTouchEnd = function(event){
+  dropdownLoading.prototype._touchEnd = function(event){
+    this.touchEnd.call(this, event, this._gap);
+    if( this._gap < 40 ){
+      this._target.style.transform = 'translate3d(0, 0, 0)';
+      this._target.style.tranition = 'transform 0.5s ease-in';
+    }else{
 
+    }
   }
 
 
-  if(typeof module !==undefined && typeof module.export === 'object'){
-    module.export = dropdownLoading;
+  if(typeof module !== 'undefined' && typeof module.exports === 'object'){
+    module.exports = dropdownLoading;
   }else{
     window.dropdownLoading = dropdownLoading;
   }
