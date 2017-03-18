40d36049931a07836611bb0ec1ef086ac2eff6be
humyfred <513407656@qq.com>
2017-03-18 21:16:50 +0800

    change

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,74 +1,53 @@
 var path = require('path')
 var config = require('../config')
-var utils = require('./utils')
-var projectRoot = path.resolve(__dirname, '../')
-
-var env = process.env.NODE_ENV
-    // check env & config/index.js to decide weither to enable CSS Sourcemaps for the
-    // various preprocessor loaders added to vue-loader at the end of this file
-var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
-var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
-var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
-
+//var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 
 module.exports = {
-    entry: {
-        app: './src/main.js'
+  entry: {
+    app : './src/index'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist/'), //文件输出目录
+    filename: '[name].js', //根据入口文件输出的对应多个文件名
+    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath //用于配置文件发布路径，如CDN或本地服务器
+  },
+  resolve: { //插件资源等等
+    extensions: ['', '.js', '.jsx', '.less', '.css', '.scss'],
+    alias: {
+      // js: path.join(__dirname, "src/scripts"),
+      // src: path.join(__dirname, "src/scripts"),
+      // styles: path.join(__dirname, "src/styles"),
+      // img: path.join(__dirname, "src/img")
     },
-    output: {
-        path: config.build.assetsRoot,
-        publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-        filename: '[name].js'
+    root: [
+      // path.join(__dirname, "bower_components")
+    ]
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    },{
+      test: /\.css$/,
+      loader: 'style!css',
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'resource/css')
     },
-    resolve: {
-        extensions: ['', '.js', '.vue', '.less', '.css', '.scss'],
-        fallback: [path.join(__dirname, '../node_modules')],
-        alias: {
-            'vue$': 'vue/dist/vue.common.js',
-            'src': path.resolve(__dirname, '../src'),
-            'assets': path.resolve(__dirname, '../src/assets'),
-            'components': path.resolve(__dirname, '../src/components')
-        }
-    },
-    resolveLoader: {
-        fallback: [path.join(__dirname, '../node_modules')]
-    },
-    module: {
-        loaders: [{
-            test: /\.vue$/,
-            loader: 'vue'
-        }, {
-            test: /\.js$/,
-            loader: 'babel',
-            include: projectRoot,
-            exclude: /node_modules/
-        }, {
-            test: /\.json$/,
-            loader: 'json'
-        }, {
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            loader: 'url',
-            query: {
-                limit: 10000,
-                name: utils.assetsPath('img/[name].[ext]')
-            }
-        }, {
-            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-            loader: 'url',
-            query: {
-                limit: 10000,
-                name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-            }
-        }]
-    },
-    vue: {
-        loaders: utils.cssLoaders({
-            sourceMap: useCssSourceMap
-        }),
-        postcss: [
-            require('autoprefixer')({
-                browsers: ['last 10 versions']
-            })
-        ]
-    }
+    {
+      test: /\.scss$/,
+      loader: "style-loader!css-loader!sass-loader",
+      exclude: /node_modules/,
+      include: __dirname,
+    },{
+      test: /\.less$/i,
+      loader: "style-loader!css-loader!less-loader",
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'resource/less')
+    }]
+  },
+  postcss: function() {
+    return [precss, autoprefixer];
+  }
 }
