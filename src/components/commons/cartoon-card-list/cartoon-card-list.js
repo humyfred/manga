430a0aded23c02039090b511b742e1511f3161b7
humyfred <513407656@qq.com>
2017-04-14 00:57:09 +0800

    add touch

--- src/components/commons/cartoon-card-list/cartoon-card-list.js
+++ src/components/commons/cartoon-card-list/cartoon-card-list.js
@@ -11,27 +11,51 @@ class CartoonCardList extends Component {
 
   init(){
     new AlloyTouch({
-            touch:"#wrapper",//反馈触摸的dom
-            vertical: true,//不必需，默认是true代表监听竖直方向touch
-            target: target, //运动的对象
-            property: "translateY",  //被运动的属性
-            min: 100, //不必需,运动属性的最小值
-            max: 2000, //不必需,滚动属性的最大值
-            sensitivity: 1,//不必需,触摸区域的灵敏度，默认值为1，可以为负数
-            factor: 1,//不必需,表示触摸位移与被运动属性映射关系，默认值是1
-            step: 45,//用于校正到step的整数倍
-            bindSelf: false,
-            initialValue: 0,
-            change:function(value){  }, //不必需，属性改变的回调。alloytouch.css版本不支持该事件
-            touchStart:function(evt, value){  },
-            touchMove:function(evt, value){  },
-            touchEnd:function(evt,value){  },
-            tap:function(evt, value){  },
-            pressMove:function(evt, value){  },
-            animationEnd:function(value){  } //运动结束
-          })
+      touch: "#wrapper",//反馈触摸的dom
+      vertical: true,//不必需，默认是true代表监听竖直方向touch
+      target: scroller, //运动的对象
+      property: "translateY",  //被滚动的属性
+      initialValue: 0,
+      min: window.innerHeight - 45 - 48 - 2000, //不必需,滚动属性的最小值
+      max: 0, //不必需,滚动属性的最大值
+      change: function (value) {
+          pull_refresh.translateY = value;
+      },
+      touchMove: function (evt, value) {
+          if (value > 70) {
+              //http://caniuse.com/#search=classList
+              arrow.classList.add("arrow_up");
+          } else {
+              arrow.classList.remove("arrow_up");
+          }
+      },
+      touchEnd: function (evt, value) {
+          if (value > 70) {
+              this.to(60);
+              mockRequest(this);
+              return false;
+          }
+      }
+    });
+
+    function mockRequest(at) {
+     pull_refresh.classList.add("refreshing");
+     setTimeout(function () {
+         var i = 0,
+             len = 3;
+         for (; i < len; i++) {
+             var li = document.createElement("li");
+             li.innerHTML = "new row " + index++;
+             list.insertBefore(li, list.firstChild);
+         }
+         arrow.classList.remove("arrow_up");
+         pull_refresh.classList.remove("refreshing");
+         at.to(at.initialValue);
+         at.min -= 40 * 3;
+     }, 500);
+   }
   }
-  
+
   componentWillMount(){
     loadData(this.props);
     Object.assign(this,initLoadingState(this.props));
