f9c76ccbf19a73de0248608fcce73294119e842e
humyfred <513407656@qq.com>
2017-04-19 23:42:40 +0800

    improve

--- src/components/commons/cartoon-card-list/cartoon-card-list.js
+++ src/components/commons/cartoon-card-list/cartoon-card-list.js
@@ -2,16 +2,21 @@
 import React,{Component, PropTypes} from 'react'
 import CartoonCard from '../cartoon-card/cartoon-card'
 import Loading from '../Loading/Loading'
-import { initLoadingState, loadData } from './cartoon-card-list-action.js'
+import { initLoadingState, loadData, loadCartoon } from './cartoon-card-list-action.js'
 import AlloyTouch from '../../../plugins/alloy_touch.css.js'
 import Transform from '../../../plugins/transform.js'
 //import DropdownLoading from '../dropdown-loading/dropdown-loading'
 
 class CartoonCardList extends Component {
 
+  constructor(prop){
+    super(prop);
+
+  }
+
   init(){
     new AlloyTouch({
-      touch: "#wrapper",//反馈触摸的dom
+      touch: "#cartoon",//反馈触摸的dom
       vertical: true,//不必需，默认是true代表监听竖直方向touch
       target: scroller, //运动的对象
       property: "translateY",  //被滚动的属性
@@ -32,7 +37,7 @@ class CartoonCardList extends Component {
       touchEnd: function (evt, value) {
           if (value > 70) {
               this.to(60);
-              mockRequest(this);
+              loadCartoon(this);
               return false;
           }
       }
@@ -41,17 +46,7 @@ class CartoonCardList extends Component {
     function mockRequest(at) {
      pull_refresh.classList.add("refreshing");
      setTimeout(function () {
-         var i = 0,
-             len = 3;
-         for (; i < len; i++) {
-             var li = document.createElement("li");
-             li.innerHTML = "new row " + index++;
-             list.insertBefore(li, list.firstChild);
-         }
-         arrow.classList.remove("arrow_up");
-         pull_refresh.classList.remove("refreshing");
-         at.to(at.initialValue);
-         at.min -= 40 * 3;
+         
      }, 500);
    }
   }
@@ -59,11 +54,11 @@ class CartoonCardList extends Component {
   componentWillMount(){
     init();
     loadData(this.props);
-    Object.assign(this,initLoadingState(this.props));
+    Object.assign(this, initLoadingState(this.props));
   }
 
   componentWillReceiveProps(props){
-    Object.assign(this,initLoadingState(props));
+    Object.assign(this, initLoadingState(props));
   }
 
   componentDidMount(){
