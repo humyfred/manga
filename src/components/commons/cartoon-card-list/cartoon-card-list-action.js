3fd762e6a389c12004030b9f5105a5510ba90acf
humyfred <513407656@qq.com>
2017-04-19 23:42:40 +0800

    improve

--- src/components/commons/cartoon-card-list/cartoon-card-list-action.js
+++ src/components/commons/cartoon-card-list/cartoon-card-list-action.js
@@ -16,3 +16,21 @@ export const loadData = (props) => {
     props.loadData();
   }
 }
+
+
+export const loadCartoon = () => {
+  pull_refresh.classList.add("refreshing");
+
+  
+  var i = 0,
+             len = 3;
+  for (; i < len; i++) {
+     var li = document.createElement("li");
+      li.innerHTML = "new row " + index++;
+      list.insertBefore(li, list.firstChild);
+  }
+    arrow.classList.remove("arrow_up");
+    pull_refresh.classList.remove("refreshing");
+    at.to(at.initialValue);
+    at.min -= 40 * 3;
+}
