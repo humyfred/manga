ee3a6456e80d1e58ddab6fbef5a915834421988a
humyfred <513407656@qq.com>
2017-08-06 20:35:41 +0800

    dllplugin modify

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,53 +1,53 @@
 var path = require('path')
 var config = require('../config')
-//var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
+    //var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 var webpack = require('webpack')
 
 
 module.exports = {
-  entry: ['./src/index.js'],
-  resolve: { //插件资源等等
-    //extensions: ['', '.js', '.jsx', '.less', '.css', '.scss'],
-    alias: {
-      // js: path.join(__dirname, "src/scripts"),
-      // src: path.join(__dirname, "src/scripts"),
-      // styles: path.join(__dirname, "src/styles"),
-      // img: path.join(__dirname, "src/img")
+    entry: ['./src/index.js'],
+    resolve: { //插件资源等等
+        //extensions: ['', '.js', '.jsx', '.less', '.css', '.scss'],
+        alias: {
+            // js: path.join(__dirname, "src/scripts"),
+            // src: path.join(__dirname, "src/scripts"),
+            // styles: path.join(__dirname, "src/styles"),
+            // img: path.join(__dirname, "src/img")
+        },
+        root: [
+            // path.join(__dirname, "bower_components")
+        ]
     },
-    root: [
-      // path.join(__dirname, "bower_components")
-    ]
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/,
-      include: path.join(__dirname, '../src')
-    },{
-      test: /\.css$/,
-      loader: 'style!css',
-      exclude: /node_modules/,
-      include: path.join(__dirname, '../src/resource/css')
-    },{
-      test: /\.scss$/,
-      loader: "style!css!sass",
-      exclude: /node_modules/,
-      include: path.join(__dirname, '../src'),
-    },{
-      test: /\.less$/i,
-      loader: "style!css!less",
-      exclude: /node_modules/,
-      include: path.join(__dirname, '../src/resource/less')
-    }]
-  },
-  plugins: [
-    new webpack.DllReferencePlugin({
-      context: __dirname, // 这里要和 dll.config.js 中 webpack.DllPlugin 配置的 context 一致
-      manifest: require('../src/manifest.json')
-    }),
-  ],
-  postcss: function() {
-    return [precss, autoprefixer];
-  }
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/,
+            include: path.join(__dirname, '../src')
+        }, {
+            test: /\.css$/,
+            loader: 'style!css',
+            exclude: /node_modules/,
+            include: path.join(__dirname, '../src/resource/css')
+        }, {
+            test: /\.scss$/,
+            loader: "style!css!sass",
+            exclude: /node_modules/,
+            include: path.join(__dirname, '../src'),
+        }, {
+            test: /\.less$/i,
+            loader: "style!css!less",
+            exclude: /node_modules/,
+            include: path.join(__dirname, '../src/resource/less')
+        }]
+    },
+    plugins: [
+        new webpack.DllReferencePlugin({      
+            context: __dirname, // 这里要和 dll.config.js 中 webpack.DllPlugin 配置的 context 一致
+            manifest: require('../dist/static/manifest.json')
+        }),
+    ],
+    postcss: function() {
+        return [precss, autoprefixer];
+    }
 }
--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -1,32 +1,32 @@
 const webpack = require('webpack');
 
 const vendors = [
-	'babel-polyfill',
-  'isomorphic-fetch',
-  'react',
-  'react-dom',
-  'react-redux',
-  'react-router-redux',
-  'react-router',
-  'redux',
-  'redux-thunk',
-  'redux-logger',
+    'babel-polyfill',
+    'isomorphic-fetch',
+    'react',
+    'react-dom',
+    'react-redux',
+    'react-router-redux',
+    'react-router',
+    'redux',
+    'redux-thunk',
+    'redux-logger',
 ];
 
 module.exports = {
-  output: {
-    path: 'src',
-    filename: '[name].dll.js',
-    library: '[name]_lib',
-  },
-  entry: {
-    vendor: vendors,
-  },
-  plugins: [
-    new webpack.DllPlugin({
-      path: 'src/manifest.json',
-      name: '[name]_lib',
-      context: __dirname
-    }),
-  ],
+    output: {
+        path: 'dist/static',
+        filename: '[name].dll.js',
+        library: '[name]_lib',
+    },
+    entry: {
+        vendor: vendors,
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: 'dist/static/manifest.json',
+            name: '[name]_lib',
+            context: __dirname
+        }),
+    ],
 };
